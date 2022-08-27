0f3e2eb3175fd301b40bc48dc423a32150b95e97
Aviral Bhardwaj <Aviral-03>
2022-08-27 15:04:58 +0530

    Final Commit

---
+++ app/src/main/java/com/labawsrh/aws/introscreen/GoalActivity.java
@@ -0,0 +1,75 @@
+package com.labawsrh.aws.introscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class GoalActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goal);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        drawer = findViewById(R.id.drawerId);
+        NavigationView navigationView = findViewById(R.id.navigationId);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.nav_open, R.string.nav_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Intent intent;
+
+        if (item.getItemId()==R.id.nav_music){
+            intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+
+        else if (item.getItemId()==R.id.nav_timer){
+            intent = new Intent(this, TimerActivity.class);
+            startActivity(intent);
+        }
+
+        else if (item.getItemId()==R.id.nav_about){
+            intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+        }
+
+        else if (item.getItemId()==R.id.nav_goal){
+            Toast.makeText(this, "Goal", Toast.LENGTH_SHORT).show();
+        }
+
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
